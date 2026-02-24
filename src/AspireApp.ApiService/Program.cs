3ef20cef0a1eafaa5f823157a46cc4f60bf210ab
MaybeMike1 <michaelberko3@gmail.com>
2026-02-24 22:32:18 +0100

    added applicationDbContext with IdentityUser for user management

--- src/AspireApp.ApiService/Program.cs
+++ src/AspireApp.ApiService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
@@ -8,8 +10,13 @@ builder.Services.AddProblemDetails();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
