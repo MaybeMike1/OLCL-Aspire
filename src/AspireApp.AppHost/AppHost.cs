1da67f663aa421a4b824d510ddd91fae002fcf85
MaybeMike1 <michaelberko3@gmail.com>
2026-02-24 21:33:52 +0100

    Added .NET Aspire

---
+++ src/AspireApp.AppHost/AppHost.cs
@@ -0,0 +1,17 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.AddViteApp(name: "todo-frontend", workingDirectory: "../AspireApp.Frontend")
+    .WithReference(apiService)
+    .WaitFor(apiService)
+    .WithNpmPackageInstallation();
+    
+builder.Build().Run();
